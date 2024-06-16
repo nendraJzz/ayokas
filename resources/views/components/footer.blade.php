9b6f740f0f0e6e030413407efb0a6298d6aa4648
nendraJzz <nendrashidiq123@gmail.com>
2024-06-16 12:53:15 +0700

    firstcommit:ayokas

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="w-full mt-10">
+    <div class="w-full border-t border-gray-500 py-5">
+        <p class="text-md font-light text-center">© 2024. All rights reserved.</p>
+    </div>
+</footer>
