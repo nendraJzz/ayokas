a9b32d727fb4ec36c07ff3055ee94d69910e8ccc
nendraJzz <nendrashidiq123@gmail.com>
2024-06-18 15:52:13 +0700

    edit:bayarkas

--- resources/views/paypage.blade.php
+++ resources/views/paypage.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="py-12 px-6">
+    <div class="py-6 px-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="w-full max-w-lg mx-auto p-2 sm:p-8">
                 <div class="bg-white rounded-lg shadow-lg p-6">
@@ -9,6 +9,14 @@
                             <div class="col-span-2 sm:col-span-1">
                                 <label for="card-number" class="block text-sm font-medium text-gray-700 mb-2">Nama</label>
                                 <input type="text" name="jumlahkas" placeholder="Nama Kamu" class="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500">
+                            </div>
+                            <div class="col-span-2 sm:col-span-1">
+                                <label for="expiration-date" class="block text-sm font-medium text-gray-700 mb-2">Nis</label>
+                                <input type="text" name="nis" placeholder="1212121" class="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500">
+                            </div>
+                            <div class="col-span-2 sm:col-span-1">
+                                <label for="expiration-date" class="block text-sm font-medium text-gray-700 mb-2">Kelas</label>
+                                <input type="text" name="jumlahkas" placeholder="XI Example" class="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500">
                             </div>
                             <div class="col-span-2 sm:col-span-1">
                                 <label for="expiration-date" class="block text-sm font-medium text-gray-700 mb-2">Jumlah Kas</label>
